66a88a0a18351cc29818d6d00f989b4dc1978dfc
Andrew Brenner <Abrenner19@gmail.com>
2021-10-15 09:35:29 -0400

    type tests and gradebook tests

--- test/GradeBook.tests/BookTests.cs
+++ test/GradeBook.tests/BookTests.cs
@@ -5,13 +5,29 @@ namespace GradeBook.tests
 {
     public class BookTests
     {
+
         [Fact]
+        public void GetsCorrectAverageLetter()
+        {
+            Book book = new Book();
+            book.AddGrade(75.82);
+            book.AddGrade(99.99);
+            book.AddGrade(62.87);
+            book.AddGrade(84.22);
+            book.AddGrade(89.73);
 
+            double avg = book.GetAvgGrade();
+            char avgLetter = book.GetAvgLetter();
 
+            Assert.Equal(82.5, avg, 1);
+            Assert.Equal('B', avgLetter);
+        }
+
+        [Fact]
         public void BookCalculatesGradeStats()
         {
             //arrange
-            var book = new Book();
+            Book book = new Book();
             book.AddGrade(69.91);
             book.AddGrade(76.27);
             book.AddGrade(99.99);
@@ -23,6 +39,7 @@ namespace GradeBook.tests
             Assert.Equal(99.99, result.High, 2);
             Assert.Equal(69.9, result.Low, 1);
             Assert.Equal(83.4, result.Average, 1);
+            Assert.Equal('B', result.AvgLetter);
         }
     }
 }
--- test/GradeBook.tests/TypeTests.cs
+++ test/GradeBook.tests/TypeTests.cs
@@ -5,14 +5,138 @@ namespace GradeBook.tests
 {
     public class TypeTests
     {
+
+
+        // String is a Reference type
+        // But, it acts very much like a value type
         [Fact]
-        public void Test1()
+        public void StringsBehaveLikeValueTypes()
         {
-            var book1 = GetBook("Book 1");
-            var book2 = GetBook("Book 2");
+            string myString = "Hello World";
+
+            MakeUpperCase(myString);
+
+            Assert.Equal("Hello World", myString);
+        }
+
+        private void MakeUpperCase(string s)
+        {
+            s.ToUpper();
+            // toUpper returns a COPY of the string in uppercase
+            // Strings are immutable
+            // Will not change the actual string, therefore it acts like a value type
+        }
+
+
+        [Fact]
+        public void CanValueTypeBeReferenced()
+        {
+            int x = GetInt();
+            SetInt(ref x);
+
+
+            Assert.Equal(42, x);
+        }
+
+        private int SetInt(ref int x)
+        {
+            x = 42;
+            return x;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        // PASS BY REFERENCE
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            Book book1 = new Book();
+
+            // Passing in the actual Reference
+            GetBookSetName(ref book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void GetBookSetName(ref Book book, string newName)
+        {
+            // Not a Copy
+            // book1 is now pointing to a new location (a new book with a new name)
+            book = new Book(newName);
+        }
+
+
+        // PASS BY VALUE
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            Book book1 = GetBook("Original Name");
+
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Original Name", book1.Name);
+        }
+
+        private void GetBookSetName(Book book, string newName)
+        {
+            book = new Book(newName);
+            // book parameter is a COPY of book1
+            // They both point to the same thing originally
+            // When you change what it POINTS to, it only affects the local parameter
+            // the "real" book1 is UNAFFECTED
+        }
+
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            Book book1 = GetBook("Original Name");
+
+            SetName(book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void SetName(Book book, string newName)
+        {
+            book.Name = newName;
+        }
+
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
+        {
+            Book book1 = GetBook("Book 1");
+            Book book2 = GetBook("Book 2");
+
+            Boolean sameBook = book1 == book2;
+            Boolean sameBookName = book1.Name == book2.Name;
+            bool x = true;
 
             Assert.Equal("Book 1", book1.Name);
             Assert.Equal("Book 2", book2.Name);
+            Assert.Equal(false, sameBook);
+            Assert.Equal(false, sameBookName);
+        }
+
+        [Fact]
+        public void TwoVariablesCanReferenceSameObject()
+        {
+
+            Book book1 = GetBook("Book 1");
+            Book book2 = book1;
+
+
+            Boolean sameBook = book1 == book2;
+            Boolean sameBookName = book1.Name == book2.Name;
+
+
+            Assert.Equal(sameBook, true);
+            Assert.Equal(sameBookName, true);
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
 
         }
 
